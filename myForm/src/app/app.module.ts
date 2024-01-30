1fffb026f4afbc57e55c866f5c51286770f59b8c
HarshThale <thaleharsh48@gmail.com>
2024-01-31 00:04:40 +0530

    commit:01
    - created new project
    - installed primeNg
    - created components login, register, home, pageNotFound
    - added service
    - added guards
    - added interfaces
    - added navigation in app-routing.module.ts file
    - <router-outlet></router-outlet>

---
+++ myForm/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
