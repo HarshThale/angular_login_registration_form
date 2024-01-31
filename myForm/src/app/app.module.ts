ea3e490a1b9f840dd2215a2d31b03732f6be6fd5
HarshThale <thaleharsh48@gmail.com>
2024-01-31 08:48:33 +0530

    commit:02
    -created email and password fields
    - added reactiveFormsModule
    - created new formGroup
    loginForm!:FormGroup
    
      ngOnInit(){
        this.loginForm = new FormGroup({
          'email': new FormControl("", [Validators.required, Validators.email]),
          'password': new FormControl("", Validators.required)
        })
      }
    
    -

--- myForm/src/app/app.module.ts
+++ myForm/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CardModule } from 'primeng/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [
     provideClientHydration()
